70112c70eb88b5b97dfd4c19cdf3a525c866edb2
Arnav Gupta <championswimmer@gmail.com>
2015-07-25 20:15:31 +0530

    set up bower and gulp for managing dependencies and deployment   [squashed set of commits]
    setting up bower to manage dependencies
    
      also have `gulp inject-imports` to import css and js
    
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>
    
    get local deploy and dist working with gulp
    
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>
    
    add minified build support with `gulp dist-min`
    
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>
    
    update README for gulp dist
    
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>
    
    move all underscores to camelCase
    
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>
    
    fix errors shown by houndci
    
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ var config = {
     "title": "OpenEvent",
     "apiBaseGetUrl": "http://open-event.herokuapp.com/get/api/v1/",
     "eventId": "3",
-    "use_testApi": true
+    "use_testApi": true 
 };
 
