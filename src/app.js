e3af8c7b0bd28fbcd664f16f0dbedf90c29d19ae
aayusharora <aayush113002@gmail.com>
2016-04-15 16:07:40 +0530

    codacy issues set2

--- src/app.js
+++ src/app.js
@@ -1,38 +1,38 @@
 /**
  * Created by championswimmer on 28/5/15.
  */
-'use strict';
+"use strict";
 // Declare app level module which depends on views, and components
 
-var openevent = angular.module('openevent',
+var openevent = angular.module("openevent",
     [
-        'ui.router',
-        'ngStorage',
-        'ngMaterial',
-        'leaflet-directive',
-        'oe.sidenav',
-        'oe.sessions',
-        'oe.speakers',
-        'oe.tracks',
-        'oe.sponsors',
-        'oe.twitterwall',
-        'oe.map'
+        "ui.router",
+        "ngStorage",
+        "ngMaterial",
+        "leaflet-directive",
+        "oe.sidenav",
+        "oe.sessions",
+        "oe.speakers",
+        "oe.tracks",
+        "oe.sponsors",
+        "oe.twitterwall",
+        "oe.map"
     ]);
 
-openevent.config(['$urlRouterProvider', '$httpProvider', '$mdThemingProvider',
+openevent.config(["$urlRouterProvider", "$httpProvider", "$mdThemingProvider",
         function($urlRouterProvider, $httpProvider, $mdThemingProvider) {
 
             //$httpProvider.defaults.useXDomain = true;
-            //delete $httpProvider.defaults.headers.common['X-Requested-With'];
-            $urlRouterProvider.otherwise('sessions');
+            //delete $httpProvider.defaults.headers.common["X-Requested-With"];
+            $urlRouterProvider.otherwise("sessions");
 
-            $mdThemingProvider.theme('default')
+            $mdThemingProvider.theme("default")
                 .primaryPalette(config.themeColorPrimary)
                 .accentPalette(config.themeColorAccent);
         }]);
 
-openevent.controller('AppController',
-    [ '$mdSidenav', '$mdMedia', '$sessionStorage', 'ApiJsonFactory',
+openevent.controller("AppController",
+    [ "$mdSidenav", "$mdMedia", "$sessionStorage", "ApiJsonFactory",
         function($mdSidenav, $mdMedia, $sessionStorage, ApiJsonFactory) {
     var app = this;
     openevent.totalDays = 0;
@@ -43,7 +43,7 @@ openevent.controller('AppController',
 
     app.$storage = $sessionStorage;
     if ( app.$storage.event === null ||
-        typeof(app.$storage.event) == 'undefined')
+        typeof(app.$storage.event) == "undefined")
     {
         app.$storage.event = [];
         app.$storage.days = [];
@@ -51,14 +51,14 @@ openevent.controller('AppController',
     app.Event = app.$storage.event;
 
     if (app.Event.length === 0) {
-        ApiJsonFactory.getJson('event')
+        ApiJsonFactory.getJson("event")
             .then(function (response) {
                 app.Event = response.data.events[0];
                 app.$storage.event = app.Event;
                 openevent.totalDays = DateUtils.DateDiff.inDays(app.Event.begin, app.Event.end) + 1;
                 app.Days = [openevent.totalDays];
                 for (var i = 0; i < openevent.totalDays; i+=1) {
-                    app.Days[i] = {num: i, label: 'Day '+ (i+1)};
+                    app.Days[i] = {num: i, label: "Day "+ (i+1)};
                 }
                 app.$storage.days = app.Days;
             }, function (error) {
