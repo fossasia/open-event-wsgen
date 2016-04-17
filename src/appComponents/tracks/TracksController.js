4934110059f17dc22496ec234b21367ef16ec727
aayusharora <aayush113002@gmail.com>
2016-04-17 20:26:31 +0530

    spaces with blocks

--- src/appComponents/tracks/TracksController.js
+++ src/appComponents/tracks/TracksController.js
@@ -1,6 +1,7 @@
 /**
  * Created by championswimmer on 29/5/15.
  */
+/* exported error */
 
 const tracksModule = angular.module("oe.tracks", ["ui.router"]);
 
@@ -20,29 +21,35 @@ tracksModule.controller("TracksController", ["$mdDialog","$sessionStorage", "$ro
 
           if ($sessionStorage.tracks === null || 
             typeof $sessionStorage.tracks === "undefined")
-        {
+         {
+
             $sessionStorage.tracks = [];
-        }
+
+          }
         tsc.Tracks = $sessionStorage.tracks;
         tsc.Sessions = $sessionStorage.sessions;
         
 
         if (tsc.Tracks.length === 0) {
-            ApiJsonFactory.getJson("tracks")
-                .then(function (response) {
-                    tsc.Tracks = response.data.tracks;
-                    $sessionStorage.tracks = tsc.Tracks;
-                }, function (error) {
+
+          ApiJsonFactory.getJson("tracks")
+            .then(function (response) {
+                tsc.Tracks = response.data.tracks;
+                $sessionStorage.tracks = tsc.Tracks;
+            }, function error (error) {
+                    //console.error(error);
+            });
+
+          ApiJsonFactory.getJson("sessions")
+            .then(function (response) {
+
+                tsc.Sessions = response.data.sessions;
+                $sessionStorage.sessions = tsc.Sessions;
+
+            }, function (error) {
                     //console.error(error);
                 });
 
-            ApiJsonFactory.getJson("sessions")
-                .then(function (response) {
-                    tsc.Sessions = response.data.sessions;
-                    $sessionStorage.sessions = tsc.Sessions;
-                }, function (error) {
-                    //console.error(error);
-                });
         }
 
         tsc.showSession = function(track, event) {
@@ -67,22 +74,26 @@ tracksModule.controller("TracksController", ["$mdDialog","$sessionStorage", "$ro
 tracksModule.controller("TrackDialogController", 
     ["$mdDialog", "$sessionStorage",
         function($mdDialog, $sessionStorage) {
+
             var tdc = this;
             tdc.track = $mdDialog.track.singleTrack;
             tdc.allSessions = $sessionStorage.sessions;
 
             tdc.count = function(track, sessions){
+
                 var count = 0;
+
                 for(var i = 0; i < sessions.length; i+=1) {
                     if(track.id === sessions[i].track) {
                         count+=1;                    
                     }                    
                 }
-
                 return count;
+
             };
             
             tdc.sessionsDetail = function(track, sessions) {
+
                 var k = 0;
                 var count = tdc.count(track,sessions);
                 var tsessions = new Array(count);
@@ -93,12 +104,14 @@ tracksModule.controller("TrackDialogController",
                         tsessions[k+=1] = sessions[i];
                     }
                 }
+                return tsessions;  
 
-                return tsessions;                
             };
 
             tdc.close = function () {
+
                 $mdDialog.hide();
+
             };
             
     }]);
