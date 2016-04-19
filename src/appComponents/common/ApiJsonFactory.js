68b52ec77e7e0dae9806539cfa1321b47f64680b
aayusharora <aayush113002@gmail.com>
2016-04-20 00:22:22 +0530

    Issues ApiJsonFactory sp]olved

--- src/appComponents/common/ApiJsonFactory.js
+++ src/appComponents/common/ApiJsonFactory.js
@@ -1,29 +1,32 @@
 /**
  * Created by championswimmer on 29/5/15.
  */
-(function() {
-    "use strict"
+(function () {
 
-const commonFactories = angular.module('openevent');
+"use strict";
+const commonFactories = angular.module("openevent");
 
-commonFactories.factory('ApiJsonFactory', ['$q', '$http', function ($q, $http) {
-let baseUrl = (config.use_testApi?'testapi/':config.apiBaseGetUrl) +
-    'event/' + config.eventId;
+commonFactories.factory("ApiJsonFactory", ["$q", "$http", function ($q, $http) {
+const baseUrl = (config.use_testApi ? "testapi/" : config.apiBaseGetUrl) +"event/" + config.eventId;
+
         return {
             getJson: function ($apiEndpoint) {
                 let endpoint ="";
-                if (($apiEndpoint === 'event') && (!config.use_testApi)) {
-                    endpoint = baseUrl;
-                } else {
-                    endpoint = baseUrl + '/' + $apiEndpoint;
-                }
+                if (($apiEndpoint === "event") && (!config.use_testApi)) {
+                 
+                 endpoint = baseUrl;
                 
+                } else {
+                 
+                 endpoint = baseUrl + "/" + $apiEndpoint;
+                
+                }
                 let deferred = $q.defer(),
                     httpPromise = $http.get(endpoint);
                 httpPromise.then(function (response) {
                     deferred.resolve(response);
                 }, function (error) {
-                    console.error(error);
+                    // console.error(error);
                 });
                 return deferred.promise;
             }
