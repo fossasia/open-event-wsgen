f3ee4a8b612bf1331682323f4ac700ccbfc064f1
aayusharora <aayush113002@gmail.com>
2016-04-21 20:25:07 +0530

    single quotes to double quotes

--- src/appComponents/twitterwall/singletweet.js
+++ src/appComponents/twitterwall/singletweet.js
@@ -14,22 +14,22 @@ function tweet($timeout,$rootScope) {
         array:"="
     },
     template: 
-            '<div class="container-tweet">'+
-            '<div class="animate" ng-repeat="photo in array">'+
-            '<div ng-if="photo.id_str===selectedid"  class="tweet">'+
-                '<img ng-class="{\'animation\':photo.id_str===selectedid}"  class="image" on-error-src="https://pbs.twimg.com/profile_images/1141238022/fossasia-cubelogo.jpg" ng-src=\'{{photo.user.profile_image_url_https}}\'>'+
-            '</div>'+
-            '<div ng-if="photo.id_str===selectedid" class="heading-tweet">'+
-                '{{photo.screen_name}}'+
-            '</div>'+
-            '<div ng-if="photo.id_str===selectedid" class="text">'+
-                '{{photo.text}}'+
-            '<div class="mention" ng-if="photo.id_str===selectedid" ng-repeat="mention in photo.mentions" class="info">'+
-                '@{{mention}}'+
-            '</div>'+
-            '</div>'+
-            '</div>'+
-            '</div>',
+            "<div class=\"container-tweet\">"+
+            "<div class=\"animate\" ng-repeat=\"photo in array\">"+
+            "<div ng-if=\"photo.id_str===selectedid\"  class=\"tweet\">"+
+                "<img ng-class=\"{\'animation\':photo.id_str===selectedid}\"  class=\"image\" on-error-src=\"https://pbs.twimg.com/profile_images/1141238022/fossasia-cubelogo.jpg\" ng-src=\"{{photo.user.profile_image_url_https}}\">"+
+            "</div>"+
+            "<div ng-if=\"photo.id_str===selectedid\" class=\"heading-tweet\">"+
+                "{{photo.screen_name}}"+
+            "</div>"+
+            "<div ng-if=\"photo.id_str===selectedid\" class=\"text\">"+
+                "{{photo.text}}"+
+            "<div class=\"mention\" ng-if=\"photo.id_str===selectedid\" ng-repeat=\"mention in photo.mentions\" class=\"info\">"+
+                "@{{mention}}"+
+            "</div>"+
+            "</div>"+
+            "</div>"+
+            "</div>",
     controller:"TwitterwallController",
     controllerAs: "vm",
     bindToController: true
