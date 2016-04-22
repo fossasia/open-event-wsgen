4694ac90a31ea47316211b49dcfcbd809423e6b7
@angularboy <aayush113002@gmail.com>
2016-04-22 19:49:31 +0530

    Codacy issues set7 (#183)
    * codacy issues set2
    
    * small fixes
    
    small fixes
    
    new generated issues
    
    app.js checks
    
    codacy issue set3
    
    codacy set 3 small conflicts
    
    small fixes
    
    small fix
    
    codacy set 4 fixes
    
    * #175 resolved my typo
    
    * codacy issues set5
    
    * codacy set6 Error Prone code
    
    * Set6 small fixes
    
    * small fixes
    
    * spaces with blocks
    
    * small fixes set6
    
    * trailing space removed
    
    * codacy small fixes set6
    
    * Codeclimate issues csslint
    
    * Strict mode fixed
    
    * small issues codeclimate
    
    * solving new issues
    
    * indentation changed from tab to space
    
    * Issues ApiJsonFactory sp]olved
    
    * concatenation removed replaced by template literal es6
    
    * small fixes Api jsonfactory
    
    * removed const ,app working
    
    * re-fixes
    
    * single quotes to double quotes
    
    * small fixes
    
    * Merge conflicts removed
    
    * small fixes codacy set 7
    
    * style issues
    
    * fixed new issues

--- src/appComponents/common/ApiJsonFactory.js
+++ src/appComponents/common/ApiJsonFactory.js
@@ -3,30 +3,28 @@
  */
 (function () {
 
-"use strict";
-const commonFactories = angular.module("openevent");
+  "use strict";
+  const commonFactories = angular.module("openevent");
 
 commonFactories.factory("ApiJsonFactory", ["$q", "$http", function ($q, $http) {
 
-var baseUrl = (config.use_testApi ? "testapi/" : config.apiBaseGetUrl) + "event/" + config.eventId;
+const baseUrl = (config.use_testApi ? "testapi/" : config.apiBaseGetUrl) + "event/" + config.eventId;
 
         return {
             getJson: function ($apiEndpoint) {
                 let endpoint = "";
 
               if ($apiEndpoint === "event" && !config.use_testApi) {
-                 
                   endpoint = baseUrl;
-                
+
               } 
               else {
-                 
                   endpoint = baseUrl + "/" + $apiEndpoint;
-                
+              
               }
               const deferred = $q.defer(),
-
               httpPromise = $http.get(endpoint);
+
               httpPromise.then(function (response) {
 
                   deferred.resolve(response);
@@ -34,12 +32,13 @@ var baseUrl = (config.use_testApi ? "testapi/" : config.apiBaseGetUrl) + "event/
               }, function (error) {
                     // console.error(error);
               });
-                return deferred.promise;
 
+            return deferred.promise;
+            
             }
 
         };
 
-    }]);
+}]);
 
 })();
--- src/appComponents/twitterwall/singletweet.js
+++ src/appComponents/twitterwall/singletweet.js
@@ -8,73 +8,85 @@ angular
 tweet.$inject=["$timeout", "$rootScope"];
 function tweet($timeout,$rootScope) {
   const directive = {
-    link: link,
-    restrict:"EA",
-    scope:{
-        array:"="
+    "link": link,
+    "restrict": "EA",
+    "scope": {
+          "array": "="
     },
-    template: 
-            '<div class="container-tweet">'+
-            '<div class="animate" ng-repeat="photo in array">'+
-            '<div ng-if="photo.id_str===selectedid"  class="tweet">'+
-                '<img ng-class="{\'animation\':photo.id_str===selectedid}"  class="image" on-error-src="https://pbs.twimg.com/profile_images/1141238022/fossasia-cubelogo.jpg" ng-src=\'{{photo.user.profile_image_url_https}}\'>'+
-            '</div>'+
-            '<div ng-if="photo.id_str===selectedid" class="heading-tweet">'+
-                '{{photo.screen_name}}'+
-            '</div>'+
-            '<div ng-if="photo.id_str===selectedid" class="text">'+
-                '{{photo.text}}'+
-            '<div class="mention" ng-if="photo.id_str===selectedid" ng-repeat="mention in photo.mentions" class="info">'+
-                '@{{mention}}'+
-            '</div>'+
-            '</div>'+
-            '</div>'+
-            '</div>',
-    controller:"TwitterwallController",
-    controllerAs: "vm",
-    bindToController: true
-        
-    };
-    return directive;
-     
-    function link(scope) {
-        scope.array="";
-        scope.getObject=getObject;
-        scope.selectedid="";
-        scope.createdAt="";
-        scope.tweetNumber=0;
-        scope.changeTweet=changeTweet;
-        scope.callchangetweet=callchangetweet;
-        scope.even=false;
+    "template": "<div class= \"container-tweet\">" +
+            "<div class= \"animate\" ng-repeat= \"photo in array\">" +
+            "<div ng-if= \"photo.id_str===selectedid \"  class= \"tweet\">" +
+                "<img ng-class= \"{\'animation\':photo.id_str===selectedid}\" class=\"image\" on-error-src=\"https://pbs.twimg.com/profile_images/1141238022/fossasia-cubelogo.jpg\" ng-src= \"{{photo.user.profile_image_url_https}}\">" +
+            "</div>" +
+            "<div ng-if= \"photo.id_str===selectedid\" class= \"heading-tweet\">" +
+                "{{photo.screen_name}}" +
+            "</div>" +
+            "<div ng-if= \"photo.id_str===selectedid\" class= \"text\">" +
+                "{{photo.text}}" +
+            "<div class= \"mention\" ng-if= \"photo.id_str===selectedid\" ng-repeat= \"mention in photo.mentions\" class= \"info\">" +
+                "@{{mention}}" +
+            "</div>" +
+            "</div>" +
+            "</div>" +
+            "</div>",
+    "controller": "TwitterwallController",
+    "controllerAs": "vm",
+    "bindToController": true    
+  };
+
+  return directive;
+
+/*  The directive link function*/
+
+  function link(scope) {
+
+    scope.array = "";
+    scope.getObject = getObject;
+    scope.selectedid = "";
+    scope.createdAt = "";
+    scope.tweetNumber = 0;
+    scope.changeTweet = changeTweet;
+    scope.callchangetweet = callchangetweet;
+    scope.even = false;
          
-        function callchangetweet(){
-                $timeout(changeTweet,8000);
-        }
-        function changeTweet(){
-            if(scope.tweetNumber!==90){
-                scope.tweetNumber=scope.tweetNumber + 1;
-                scope.selectedid=scope.array[scope.tweetNumber].id_str;
-                $rootScope.$broadcast("selectedid",scope.selectedid);
-                callchangetweet();
-            }
-            else {
-                scope.tweetNumber=0;    
-                callchangetweet();
-            }
-            }
-       
-        function getObject(){
-                scope.$watch("array",function(){
-                if(scope.array){
-                    scope.selectedid=scope.array[1].id_str;
-                    changeTweet();
-                }
-            });
-        }
-        getObject();
-        scope.$on("array",function(args,message){
-            scope.array=message;
-        });
-           
-        }
+    function callchangetweet() {
+          $timeout(changeTweet,8000);
+
     }
+    function changeTweet() {
+
+      if (scope.tweetNumber !=== 90) {
+            scope.tweetNumber = scope.tweetNumber + 1;
+            scope.selectedid  =scope.array[scope.tweetNumber].id_str;
+            $rootScope.$broadcast("selectedid",scope.selectedid);
+            callchangetweet();
+
+      }
+      else {
+            scope.tweetNumber=0;    
+            callchangetweet();
+
+      }
+    }
+   
+    function getObject() {
+
+      scope.$watch("array",function(){
+
+      if (scope.array) {
+        scope.selectedid=scope.array[1].id_str;
+        changeTweet();
+
+        }
+
+      });
+
+    }
+    getObject();
+    scope.$on("array",function(args,message){
+      scope.array=message;
+
+    });
+       
+    }
+}
