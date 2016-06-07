c22ad3766a49ff7cc91f2780fe40ffcb9a07377e
Arnav Gupta <championswimmer@gmail.com>
2016-06-08 03:51:16 +0530

    configure for heroku
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- src/generator/app.js
+++ src/generator/app.js
@@ -2,16 +2,18 @@
 var express = require('express');
 var app = express();
 
-var generator = require('./backend/generator.js')
+var generator = require('./backend/generator.js');
+
+app.set('port', (process.env.PORT || 5000));
 
 // Use the www folder as static frontend
 app.use('/', express.static(__dirname + '/www'));
 
 app.all('/generate', function (req, res) {
-  res.setHeader('Content-Type', 'application/zip')
+  res.setHeader('Content-Type', 'application/zip');
   res.send(generator.getSchedulePage().toBuffer());
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
