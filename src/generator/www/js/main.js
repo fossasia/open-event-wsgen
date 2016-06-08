964e25e9487d944b99b503a3d037e2036f0f5260
aayusharora <aayush113002@gmail.com>
2016-06-08 14:45:11 +0300

    Hound errors

--- src/generator/www/js/main.js
+++ src/generator/www/js/main.js
@@ -1,36 +1,37 @@
 $(document).ready(function() {
 
-  var $email =$('#email');
-  var $name  =$('#name'); 
-  var $url  =$('#url');
+  let $email =$("#email");
+  let $name  =$("#name"); 
+  let $url  =$("#url");
 
-  $('#btn').click(function() { 
+  $("#btn").click(function() { 
 
-  var form = document.querySelector('form');
+  var form = document.querySelector("form");
   var fd = new FormData(form);
 
   var files ={
-    'sessionfile':  $( '#sessionfile' )[0].files[0],
-    'speakerfile':$( '#speakerfile' )[0].files[0],
-    'trackfile':$( '#trackfile' )[0].files[0] ,
-    'locationfile':$( '#locationfile' )[0].files[0]
+    "sessionfile":  $( "#sessionfile" )[0].files[0],
+    "speakerfile":$( "#speakerfile" )[0].files[0],
+    "trackfile":$( "#trackfile" )[0].files[0] ,
+    "locationfile":$( "#locationfile" )[0].files[0]
   }
   
   for (var key in files) {
     //console.log(key, files[key]);
     fd.append(key, files[key]);
   }
+
   var obj={
     name:$name.val(),
     email:$email.val(),
     url :$url.val(),
     files:fd
     
-  }
+  };
 
   $.ajax({
-    type: 'POST',
-    url:  ' ',
+    type: "POST",
+    url:  " ",
     data: obj,
     processData: false,
     contentType: false,
