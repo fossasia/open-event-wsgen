c785ef7998e1e13cd0859d123234188d2afee321
Arnav Gupta <championswimmer@gmail.com>
2016-07-11 09:32:37 +0530

    add test for social links
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- test/serverTest.js
+++ test/serverTest.js
@@ -2,7 +2,9 @@
  * Created by championswimmer on 10/7/16.
  */
 
-/*eslint no-undef: 0*/
+/* eslint no-undef: 0 */
+
+'use strict';
 
 var assert = require('chai').assert;
 const jsonfile = require('jsonfile');
@@ -12,7 +14,9 @@ var generator = require('../src/generator/backend/generator.js');
 var dist = require('../src/generator/backend/dist.js');
 
 var data = {
-  event: jsonfile.readFileSync(__dirname + '/../mockjson/event')
+  event: jsonfile.readFileSync(__dirname + '/../mockjson/event'),
+  sponsors: jsonfile.readFileSync(__dirname + '/../mockjson/sponsors'),
+  sessions: jsonfile.readFileSync(__dirname + '/../mockjson/sessions')
 };
 
 
@@ -26,6 +30,17 @@ describe('fold', function() {
   describe('.getAppName()', function () {
     it('should return event title from event object', function () {
       assert.equal(fold.getAppName(data.event), 'Open Tech Summit')
+    })
+  });
+  describe('.createSocialLinks()', function () {
+    it('should return array of social links of event', function () {
+      var socialLinks = fold.createSocialLinks(data.event);
+      assert.equal(socialLinks[0].icon, 'twitter');
+      assert.equal(socialLinks[1].icon, 'github');
+      assert.equal(socialLinks[2].icon, 'facebook');
+      assert.equal(socialLinks[3].icon, 'flickr');
+      assert.equal(socialLinks[4].icon, 'google-plus');
+      assert.equal(socialLinks[5].icon, 'youtube-play');
     })
   })
 });
