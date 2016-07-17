15a5932a8304f03cb0afb5b4dc125df11f6e7b03
aayusharora <aayush113002@gmail.com>
2016-07-18 01:55:49 +0530

    UX improvement tracks page

--- src/generator/backend/assets/js/popover.js
+++ src/generator/backend/assets/js/popover.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
   var popbox = $('.pop-box');
   popbox.hide();
   if($(window).width() < 768) {
-    $('.item').click(function(event) {
+    $('.sizeevent').click(function(event) {
       popBox(event);
     });
   }
   else {
-    $('.item').hover(function(event) {
+    $('.sizeevent').hover(function(event) {
       popBox(event);
     });
   }
@@ -19,9 +19,9 @@ $(document).ready(function() {
     popbox.hide();
     event.preventDefault();
     event.stopPropagation();
-
-    var track = $(event.target);
-    var link  = track.children(0);
+    
+   var track = $(event.target);
+     /*var link  = track.children(0);
 
     if($(link).offset() === undefined) {
       var offset = $(track).offset();
@@ -32,34 +32,36 @@ $(document).ready(function() {
     }
 
     var nextOfpop = $(track).next();
-    var position = offset.top - link.height() - 30;
-    var left = offset.left;
+    //var position = offset.top - link.height() - 30;
+    //var left = offset.left;
 
     if($(window).width() >= 320 && $(window).width() < 481) {
-      var position = offset.top - link.height() - 15;
+      //var position = offset.top - link.height() - 15;
     }
     else if($(window).width() >= 481 && $(window).width() < 597) {
-      var position = offset.top - link.height() - 20;
+      //var position = offset.top - link.height() - 20;
     }
     else if($(window).width() >= 597 && $(window).width() < 641) {
-      var position = offset.top - link.height() - 46;
+      //var position = offset.top - link.height() - 46;
     }
     else if($(window).width() >= 641 && $(window).width() < 961) {
-      var position = offset.top - link.height() - 22;
+      //var position = offset.top - link.height() - 22;
     }
     else if($(window).width() >= 961 && $(window).width() < 1025) {
-      var left = left - 180;
+      //var left = left - 180;
     }
     else {
-      var left = left - 248;
+      //var left = left - 248;
     }
-    if(offset.top) {
-      var toptrack = position;
-      $(nextOfpop).css({'top': toptrack,
-                      'left': left
-    });
-      $(track).next().show();
+   if(offset.top) {
+      //var toptrack = position;
+      //$(nextOfpop).css({'top': toptrack,
+                      //'left': left
+    //});
+    
     }
+*/
+  $(track).next().show();
     $(document).mouseup(function(e) {
       var container = popbox;
 
