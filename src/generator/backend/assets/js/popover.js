f96653aedc4755bc1d99821129226791ec96a2fb
aayusharora <aayush113002@gmail.com>
2016-07-21 05:19:29 +0530

    Bug fix track

--- src/generator/backend/assets/js/popover.js
+++ src/generator/backend/assets/js/popover.js
@@ -1,16 +1,22 @@
 /* Created by aayusharora on June 20, 2017 */
 
 $(document).ready(function() {
+  var widthWindow = $(window).width();
   var popbox = $('.pop-box');
+  var sizeevent = $('.sizeevent');
+  var tracktime = $('.sizeevent span'); 
+
   popbox.hide();
-  if($(window).width() < 768) {
+  if( widthWindow < 768) {
     $('.sizeevent').click(function(event) {
       popBox(event);
     });
   }
   else {
-    $('.sizeevent').hover(function(event) {
+    $('.header-pop').hover(function(event) {
       popBox(event);
+    },function(){
+      popbox.hide();
     });
   }  
   function popBox(e) {
@@ -20,12 +26,27 @@ $(document).ready(function() {
     event.preventDefault();
     event.stopPropagation();
     
-   var track = $(event.target);
+    var sizecontainer = sizeevent;
+    var timeOftrack = tracktime;
+    if(!sizecontainer.is(event.target)) {
+      if(!timeOftrack.is(event.target)){
+        var track = $(event.target).children('h4');
+      }
+      else {
+        var track = $(event.target).parent();
+      }
+      
+    }
+    else {
+      var track = $(event.target);
+    }
+    
+   console.log(event.target);
    var tracknext= $(track).next();
     tracknext.show();
    var tracktocheck= track.offset().top + track.outerHeight() + tracknext.outerHeight() + 15;
    var shift= tracktocheck - ($('.main').offset().top + $('.main').outerHeight());
-   if(shift>0){
+   if(shift > 0){
    
     $('.footer').css({
       'position':'absolute',
@@ -39,17 +60,18 @@ $(document).ready(function() {
       'position':'static'
     })
   }
+
  
- $(document).mouseup(function(e) {
+  }
+  if (widthWindow < 768) {
+   $(document).mouseup(function(e) {
       var container = popbox;
-      
       if (!container.is(e.target)
-            && container.has(e.target).length === 0 && (e.target) !== $('html').get(0))
+            && container.has(e.target).length === 0)
           {
         container.hide();
     
       }
     });
-  }
-   
+  } 
 });
