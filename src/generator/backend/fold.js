d882383ad799deeb9202497bab06c5929cba9de7
aayusharora <aayush113002@gmail.com>
2016-08-01 20:15:41 +0530

    sponsors added

--- src/generator/backend/fold.js
+++ src/generator/backend/fold.js
@@ -215,8 +215,22 @@ function getCopyrightData(event) {
 
 function foldByLevel(sponsors ,reqOpts) {
   let levelData = {};
-
+  let level1=0,level2=0,level3=0;
   const appFolder = reqOpts.email + '/' + slugify(reqOpts.name);
+  sponsors.forEach( (sponsor) => {
+    if(sponsor.level==="1" && (sponsor.logo !== null||" ")){
+      level1++;
+    }
+    if (sponsor.level==="2" && (sponsor.logo !== null||" ")) {
+       level2++;
+    }
+     if (sponsor.level==="3" && (sponsor.logo !== null||" ")) {
+       level3++;
+    }
+
+  });
+  console.log(level2);
+  console.log(level1);
   sponsors.forEach((sponsor) => {
     if (levelData[sponsor.level] === undefined) {
       levelData[sponsor.level] = [];
@@ -244,16 +258,37 @@ function foldByLevel(sponsors ,reqOpts) {
 
     switch (sponsorItem.level) {
       case '1':
+      if(level1 === 1) {
         sponsorItem.divclass = 'largeoffset col-md-4';
+      }
+      else if(level1 === 2) {
+        sponsorItem.divclass = 'sublargeoffset col-md-4';
+      }
+      else {
+        sponsorItem.divclass = 'col-md-4';
+      }
         sponsorItem.imgsize = 'large';
         break;
       case '2':
       default:
+       if( level2 > 0 && level2 < 6 ) {
         sponsorItem.divclass = 'mediumoffset col-md-2';
+      }
+      else {
+        sponsorItem.divclass = 'col-md-2';
+      }
         sponsorItem.imgsize = 'medium';
         break;
       case '3':
-        sponsorItem.divclass = 'smalloffset col-md-2';
+      if (level3 === 1) {
+         sponsorItem.divclass = 'smalloffset col-md-2';
+      }
+      else if( level3 >1 && level3 < 5){
+        sponsorItem.divclass = 'mediumoffset col-md-2';
+      }
+      else {
+         sponsorItem.divclass = 'col-md-2';
+      }
         sponsorItem.imgsize = 'small';
         break;
     }
