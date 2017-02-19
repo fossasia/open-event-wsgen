11ef7feaaf3c4059949702e8c0636b62045cf16e
Princu7 <princu7@gmail.com>
2017-02-19 11:50:37 +0530

    Add unit test to confirm the generation of the event site

--- src/backend/buildlogger.js
+++ src/backend/buildlogger.js
@@ -3,12 +3,19 @@ var exports = module.exports = {};
 var buildLog = [];
 
 exports.addLog = function(type, smallMessage, socket, largeMessage) {
-    if (typeof(largeMessage) === 'undefined') {
-        largeMessage = smallMessage;
-    }
+  if (typeof(largeMessage) === 'undefined') {
+    largeMessage = smallMessage;
+  }
 
-    buildLog.push({'type':type, 'smallMessage' : smallMessage, 'largeMessage': largeMessage});
-    largeMessage = largeMessage.toString();
-    var obj = {'type' : type, 'smallMessage' : smallMessage, 'largeMessage': largeMessage};
+  buildLog.push({'type':type, 'smallMessage' : smallMessage, 'largeMessage': largeMessage});
+  largeMessage = largeMessage.toString();
+  var obj = {'type' : type, 'smallMessage' : smallMessage, 'largeMessage': largeMessage};
+  var emit = false;
+
+  if (socket.constructor.name === 'Socket') {
+    emit = true;
+  }
+  if (emit) {
     socket.emit('buildLog', obj);
+  }
 };
