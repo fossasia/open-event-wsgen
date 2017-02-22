ed11f70ecb5228086107009506e8458f297a9928
@angularboy <aayush113002@gmail.com>
2017-02-23 02:37:40 +0530

    Updated event sample

--- sample_cli.sh
+++ sample_cli.sh
@@ -4,6 +4,6 @@ set -o errexit -o nounset
 
 HSAMPLE_TIMEOUT="${HSAMPLE_TIMEOUT:-60s}"
 
-HSAMPLE_ENDPOINT="${HSAMPLE_ENDPOINT:-https://raw.githubusercontent.com/fossasia/open-event/master/sample/OTS16/}"
+HSAMPLE_ENDPOINT="${HSAMPLE_ENDPOINT:-https://eventyay.com/api/v1/events/6}"
 
 node cli.js heroku_dir $HSAMPLE_ENDPOINT & timeout $HSAMPLE_TIMEOUT npm run start;
