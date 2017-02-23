e8b295f352818bec65eef14b934de3d7790da0ad
Princu7 <princu7@gmail.com>
2017-02-23 19:41:14 +0530

    Enable automatic deployment of FOSSASIA Summit on gh-pages on each PR merge

--- cli.js
+++ cli.js
@@ -8,7 +8,7 @@ var client = io.connect('http://127.0.0.1:5000');
 var i = 1;
 client.on('connect', function () {
 
-    if (i == 1) {
+    if (i === 1) {
         i = 0;
         var data = {
             name: 'testapp',
@@ -17,10 +17,8 @@ client.on('connect', function () {
             datasource: 'eventapi',
             assetmode: 'download',
             apiendpoint: process.argv[3] || "https://eventyay.com/api/v1/events/6" || process.env.GH_ENDPOINT
-        }
+        };
         client.emit('live', data);
     }
     client.close();
 });
-
-
