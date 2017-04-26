282e9d7d71870ab4efdce786181f803311d5a561
Princu7 <princu7@gmail.com>
2017-04-26 18:00:24 +0530

    Increased the scroll speed

--- src/backend/assets/js/scroll.js
+++ src/backend/assets/js/scroll.js
@@ -1,30 +1,30 @@
       $(function() {
       $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
         var href = $(this).attr('href');
-        var link = href.split('#')[1]; 
+        var link = href.split('#')[1];
         var selector = "a[id='" + link + "']";
         var target = $(selector);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
           $('html, body').animate({
             scrollTop: target.offset().top
-            }, 1000);
+            }, 500);
             return false;
           }
         }
       });
     $('#down-button').hide();
     $('#down-button').click(function() {
-      $('html, body').animate({scrollTop: 0}, 1000);
-      $('#down-button').fadeOut(1000);
+      $('html, body').animate({scrollTop: 0}, 500);
+      $('#down-button').fadeOut(500);
     });
     window.addEventListener('scroll', function() {
       if($(window).scrollTop() > 0) {
-        $('#down-button').fadeIn(1000);
+        $('#down-button').fadeIn(500);
       }
       if($(window).scrollTop() === 0) {
-        $('#down-button').fadeOut(1000);
+        $('#down-button').fadeOut(500);
       }
     });
     });
