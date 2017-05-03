21b546cfdaf1b5494ae37de2c8908e6d772465f3
Dilpreet Singh <singhsince94@gmail.com>
2017-05-03 05:43:39 +0530

    Changes behavior of day buttons in tracks & rooms

--- src/backend/assets/js/tabs.js
+++ src/backend/assets/js/tabs.js
@@ -3,20 +3,24 @@ var tabs, i, myTabs;
 
 tabs = function(options) {
   var goToTab, handleClick, init;
-  var el = document.querySelector(options.el);
-  var tabNavigationLinks = el.querySelectorAll(options.tabNavigationLinks);
-  var tabContentContainers = el.querySelectorAll(options.tabContentContainers);
+  var el = $(options.el);
+  var tabNavigationLinks = $(el).find(options.tabNavigationLinks);
+  var tabContentContainers = $(el).find(options.tabContentContainers);
   var activeIndex = -1;
   var initCalled = false;
+  var date = $('.date-filter').first();
 
   goToTab = function(index) {
     if (index !== activeIndex && index >= 0 && index <= tabNavigationLinks.length) {
       if(activeIndex >= 0) {
-        tabNavigationLinks[activeIndex].classList.remove('is-active');
-        tabContentContainers[activeIndex].classList.remove('is-active');
+        $(tabNavigationLinks[activeIndex]).removeClass('is-active');
+        $(tabContentContainers[activeIndex]).removeClass('is-active');
       }
-      tabNavigationLinks[index].classList.add('is-active');
-      tabContentContainers[index].classList.add('is-active');
+      date = $('.' + $(tabNavigationLinks[index]).attr('id'));
+      $('.date-filter').addClass('hide');
+      $(date).removeClass('hide');
+      $(tabNavigationLinks[index]).addClass('is-active');
+      $(tabContentContainers[index]).addClass('is-active');
       activeIndex = index;
     }
   };
@@ -28,10 +32,11 @@ tabs = function(options) {
   };
   init = function() {
     var link;
+    $(date).show();
 
     if (!initCalled) {
       initCalled = true;
-      el.classList.remove('no-js');
+      $(el).removeClass('no-js');
       for (i = 0; i < tabNavigationLinks.length; i++) {
         link = tabNavigationLinks[i];
         handleClick(link, i);
