be9ab3a2d90e9425000c95c1d3b4c6b6577c43f7
Princu7 <princu7@gmail.com>
2017-07-10 19:11:12 +0530

    write string form of functions

--- src/selenium/basePage.js
+++ src/selenium/basePage.js
@@ -65,20 +65,11 @@ var BasePage = {
   },
 
   checkDownButton: function() {
-
-    function scrollToEndOfPage() {
-      window.scrollTo(0, document.body.scrollHeight);
-    }
-
-    function getVerticalOffset() {
-      return window.scrollY;
-    }
-
     var self = this;
 
-    return self.driver.executeScript(scrollToEndOfPage).then(self.find.bind(self, By.id('down-button'))).then(function(el) {
+    return self.driver.executeScript('window.scrollTo(0, document.body.scrollHeight)').then(self.find.bind(self, By.id('down-button'))).then(function(el) {
       return el.click().then(self.driver.sleep(1000)).then(function() {
-        return self.driver.executeScript(getVerticalOffset);
+        return self.driver.executeScript('return window.scrollY');
       });
     });
   }
