0eb024e75dbc427171ba14fbc597caf4fc0a812f
Princu7 <princu7@gmail.com>
2017-07-11 18:01:29 +0530

    Add bookmark test to Schedule and Rooms Page

---
+++ src/selenium/schedulePage.js
@@ -0,0 +1,29 @@
+var BasePage = require('./basePage.js');
+var By = require('selenium-webdriver').By;
+var until = require('selenium-webdriver').until;
+
+var SchedulePage = Object.create(BasePage);
+
+SchedulePage.checkIsolatedBookmark = function() {
+
+  // We go into starred mode and unmark sessions having id 3014 which was marked previously on tracks pages. If the bookmark feature works, then length of the web page would decrease.
+
+  var self = this;
+  var getPageHeight = 'return document.body.scrollHeight';
+  var sessionIdsArr = ['3014'];
+  var oldHeight, newHeight;
+
+  return self.toggleStarredButton().then(function() {
+    return self.driver.executeScript(getPageHeight).then(function(height) {
+      oldHeight = height;
+      return self.toggleSessionBookmark(sessionIdsArr).then(function() {
+        return self.driver.executeScript(getPageHeight).then(function(height) {
+          newHeight = height;
+          return oldHeight > newHeight;
+        });
+      });
+    });
+  });
+};
+
+module.exports = SchedulePage;
