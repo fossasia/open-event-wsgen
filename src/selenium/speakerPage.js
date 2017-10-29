80fa0048dac5c730fe7afcebde4262893fade64e
Saurabh Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-10-30 05:20:01 +0530

    Fix Bad Grammar (#1517)

--- src/selenium/speakerPage.js
+++ src/selenium/speakerPage.js
@@ -1,11 +1,11 @@
 var BasePage = require('./basePage.js');
 var By = require('selenium-webdriver').By;
-var until = require('selenium-webdriver').until;
 
 var SpeakerPage = Object.create(BasePage);
 
 SpeakerPage.searchTest = function() {
   var idList = ['2330', '2240'];
+  
   return this.commonSearchTest('Mario', idList);
 };
 
@@ -20,13 +20,33 @@ SpeakerPage.jumpToTrack = function() {
       elem.findElement(By.className('sessions')).findElement(By.css('a')).click().then(self.getPageUrl.bind(self))
         .then(function(url) {
           self.driver.executeScript(pageVertScrollOffset).then(function(height) {
-            resolve(height > 0 && (url.search('tracks') != -1));
+            resolve(height > 0 && (url.search('tracks') !== -1));
         });
       });
     });
   });
 
   return trackPromise;
+};
+
+SpeakerPage.jumpToSession = function() {
+  var self = this;
+  var speakerId = '53';
+  var pageVertScrollOffset = 'return window.scrollY';
+  
+  var sessionPromise =  new Promise(function(resolve) {
+    self.find(By.id(speakerId)).then(function(elem) {
+      self.driver.actions().mouseMove(elem).perform();
+      elem.findElement(By.className('sessions')).findElement(By.css('a')).click().then(self.getPageUrl.bind(self))
+        .then(function(url) {
+          self.driver.executeScript(pageVertScrollOffset).then(function() {
+            resolve(url.search('session') !== -1);
+          });
+        });
+    });
+  });
+  
+  return sessionPromise;
 };
 
 module.exports = SpeakerPage;
