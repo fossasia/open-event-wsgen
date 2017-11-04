315e00b3772d17df1978aa63d6a4b212174b8d89
Ayush Gupta <iec2016039@iiita.ac.in>
2017-11-04 20:12:20 +0530

    Added RedHatSummit 2017 to live site

--- test/serverTest.js
+++ test/serverTest.js
@@ -332,9 +332,27 @@ describe('generate', function () {
       
     });
     
+    it('should generate the RedHat Summit 2017 event', function(done) {
+      var data = {};
+    
+      data.body = {
+        "email": "a@a.com",
+        "name": "Open Event",
+        "apiendpoint": "https://raw.githubusercontent.com/fossasia/open-event/master/sample/RedHatSummit17",
+        "datasource": "eventapi",
+        "assetmode": "download"
+      };
+    
+      generator.createDistDir(data, 'Socket', function (appFolder) {
+        assert.equal(appFolder, "a@a.com/RedHatSummit");
+        done();
+      });
+    
+    });
+    
     it('should copy all the static files', function (done) {
       var staticPath = __dirname + '/../src/backend/overviewSite/';
-      var totalFiles = 10;
+      var totalFiles = 11;
       var counter = 0;
       
       function copyStatic(fileName) {
@@ -366,6 +384,7 @@ describe('generate', function () {
       copyStatic('nextcloud2017.jpg');
       copyStatic('googleIO.jpg');
       copyStatic('PyCon17.jpg');
+      copyStatic('redhat.jpg');
       
     });
     
