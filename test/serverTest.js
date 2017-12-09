7628f6dae1838d2c73fc23be3a8d52f6f5772a98
Parth <24358501+ParthS007@users.noreply.github.com>
2017-12-09 23:55:18 +0530

    Fixes#1530 Updation of URL links and logo of other projects in Menu item (#1531)

--- test/serverTest.js
+++ test/serverTest.js
@@ -222,10 +222,27 @@ describe('generate', function () {
       
     });
     
+    it('should generate the RedHat Summit 2017 event', function(done) {
+      var data = {};
+    
+      data.body = {
+        "email": "a@a.com",
+        "name": "Open Event",
+        "apiendpoint": "https://raw.githubusercontent.com/fossasia/open-event/master/sample/RedHatSummit17",
+        "datasource": "eventapi",
+        "assetmode": "download"
+      };
+    
+      generator.createDistDir(data, 'Socket', function (appFolder) {
+        assert.equal(appFolder, "a@a.com/RedHatSummit");
+        done();
+      });
+    
+    });
+    
     it('should copy all the static files', function (done) {
       var staticPath = __dirname + '/../src/backend/overviewSite/';
-      var counter = 0;
-      
+
       function copyStatic(fileName) {
         fs.readFile(staticPath + fileName, function (err, data) {
           if (err) {
@@ -255,6 +272,7 @@ describe('generate', function () {
       copyStatic('nextcloud2017.jpg');
       copyStatic('googleIO.jpg');
       copyStatic('PyCon17.jpg');
+      copyStatic('redhat.jpg');
       copyStatic('droidcon.jpg');
       
     });
