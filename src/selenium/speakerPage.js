06e4b7f89d7886ff7e06b6319b8d33e4c2aed096
Ayush Gupta <iit2016508@iiita.ac.in>
2018-01-15 18:26:39 +0530

    Added hover on image test for speakers page (#1632)

--- src/selenium/speakerPage.js
+++ src/selenium/speakerPage.js
@@ -5,7 +5,7 @@ var SpeakerPage = Object.create(BasePage);
 
 SpeakerPage.searchTest = function() {
   var idList = ['2330', '2240'];
-  
+
   return this.commonSearchTest('Mario', idList);
 };
 
@@ -33,7 +33,7 @@ SpeakerPage.jumpToSession = function() {
   var self = this;
   var speakerId = '53';
   var pageVertScrollOffset = 'return window.scrollY';
-  
+
   var sessionPromise =  new Promise(function(resolve) {
     self.find(By.id(speakerId)).then(function(elem) {
       self.driver.actions().mouseMove(elem).perform();
@@ -45,8 +45,28 @@ SpeakerPage.jumpToSession = function() {
         });
     });
   });
-  
+
   return sessionPromise;
+};
+
+SpeakerPage.hoverOverSpeaker = function() {
+  var self = this;
+  var speaker = 'speaker';
+  var speakerDetailsDiv = 'speaker-social-icons';
+
+  var promise = new Promise(function(resolve) {
+    self.find(By.className(speaker)).click().then(self.driver.sleep(1000)).then(function() {
+      var socialLinkPromise = self.getAllLinks(By.id(speakerDetailsDiv));
+      
+      socialLinkPromise.then(function(socialLinkArr) {
+        var brokenLinks = self.countBrokenLinks(socialLinkArr.slice(0, 4));
+        
+        resolve(brokenLinks);
+      });
+    });
+  });
+
+  return promise;
 };
 
 module.exports = SpeakerPage;
