98c79da2106a552b09164dc84594e51ec6cfe9d4
shubham <kumarsinghshubham32111@gmail.com>
2018-02-11 13:31:49 +0530

    Improve menu of Web app generator

--- src/www/js/form.js
+++ src/www/js/form.js
@@ -97,19 +97,27 @@ $(document).ready(function () {
 
   });
 
-  var customMenuButton = $('.glyphicon-th').first();
-  var menuContent = $('.custom-menu-cont');
+    var customMenuButton = $('.custom-menubutton');
+    var menuContent = $('.custom-menu-cont');
+    var fontAwesomeIcom=$('.glyphicon-th');
 
-  customMenuButton.click(function() {
-    menuContent.toggleClass("hidden");
-  });
+    customMenuButton.click(function() {
+        menuContent.toggleClass("hidden");
+        $(this).toggleClass('custom-menubutton-color');
+    });
+
+    $('.custom-menu-item').click(function () {
+        menuContent.addClass('hidden');
+        customMenuButton.removeClass('custom-menubutton-color');
+    });
 
   $(document).mouseup(function(e) {
     // if the target of the click is not the button,
     // the container, or descendants of the container
-    if (!customMenuButton.is(e.target) && !menuContent.is(e.target) && menuContent.has(e.target).length === 0) {
-      menuContent.addClass("hidden");
-    }
+      if (!$(e.target).is(customMenuButton) && !$(e.target).is(menuContent) && menuContent.has(e.target).length === 0 && !$(e.target).is(fontAwesomeIcom)) {
+          menuContent.addClass("hidden");
+          customMenuButton.removeClass('custom-menubutton-color')
+      }
   });
 
   $('#cancelUpload').click(function(e){
