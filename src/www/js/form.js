c6393ca45fa55e4128d17ce4be01ed1fe31715a4
shubham <kumarsinghshubham32111@gmail.com>
2018-02-12 00:27:43 +0530

    indented code

--- src/www/js/form.js
+++ src/www/js/form.js
@@ -97,27 +97,27 @@ $(document).ready(function () {
 
   });
 
-    var customMenuButton = $('.custom-menubutton');
-    var menuContent = $('.custom-menu-cont');
-    var fontAwesomeIcom=$('.glyphicon-th');
+  var customMenuButton = $('.custom-menubutton');
+  var menuContent = $('.custom-menu-cont');
+  var fontAwesomeIcom=$('.glyphicon-th');
 
-    customMenuButton.click(function() {
-        menuContent.toggleClass("hidden");
-        $(this).toggleClass('custom-menubutton-color');
-    });
+  customMenuButton.click(function() {
+    menuContent.toggleClass("hidden");
+    $(this).toggleClass('custom-menubutton-color');
+  });
 
-    $('.custom-menu-item').click(function () {
-        menuContent.addClass('hidden');
-        customMenuButton.removeClass('custom-menubutton-color');
-    });
+  $('.custom-menu-item').click(function () {
+     menuContent.addClass('hidden');
+     customMenuButton.removeClass('custom-menubutton-color');
+  });
 
   $(document).mouseup(function(e) {
     // if the target of the click is not the button,
     // the container, or descendants of the container
-      if (!$(e.target).is(customMenuButton) && !$(e.target).is(menuContent) && menuContent.has(e.target).length === 0 && !$(e.target).is(fontAwesomeIcom)) {
-          menuContent.addClass("hidden");
-          customMenuButton.removeClass('custom-menubutton-color')
-      }
+    if (!$(e.target).is(customMenuButton) && !$(e.target).is(menuContent) && menuContent.has(e.target).length === 0 && !$(e.target).is(fontAwesomeIcom)) {
+      menuContent.addClass("hidden");
+      customMenuButton.removeClass('custom-menubutton-color')
+    }
   });
 
   $('#cancelUpload').click(function(e){
