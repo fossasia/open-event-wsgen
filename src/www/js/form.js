3d3fb431d6d94f85ec43c4e9b72bc77b0198dbfb
shubham3211 <kumarsinghshubham32111@gmail.com>
2018-02-13 18:39:33 +0530

    Improve menu of Web app generator (#1712)
    * Improve menu of Web app generator
    
    * Improved menu of Web app generator
    
    * indented the code
    
    * indented code

--- src/www/js/form.js
+++ src/www/js/form.js
@@ -97,18 +97,26 @@ $(document).ready(function () {
 
   });
 
-  var customMenuButton = $('.glyphicon-th').first();
+  var customMenuButton = $('.custom-menubutton');
   var menuContent = $('.custom-menu-cont');
+  var fontAwesomeIcom=$('.glyphicon-th');
 
   customMenuButton.click(function() {
     menuContent.toggleClass("hidden");
+    $(this).toggleClass('custom-menubutton-color');
+  });
+
+  $('.custom-menu-item').click(function () {
+     menuContent.addClass('hidden');
+     customMenuButton.removeClass('custom-menubutton-color');
   });
 
   $(document).mouseup(function(e) {
     // if the target of the click is not the button,
     // the container, or descendants of the container
-    if (!customMenuButton.is(e.target) && !menuContent.is(e.target) && menuContent.has(e.target).length === 0) {
+    if (!$(e.target).is(customMenuButton) && !$(e.target).is(menuContent) && menuContent.has(e.target).length === 0 && !$(e.target).is(fontAwesomeIcom)) {
       menuContent.addClass("hidden");
+      customMenuButton.removeClass('custom-menubutton-color')
     }
   });
 
