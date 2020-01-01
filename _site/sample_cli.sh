5a80755eeb8c54324fbbc3ae6bbe1ee64bb73744
pranavkarthik10 <pranav.karthik@icloud.com>
2019-12-31 16:03:10 -0800

    Remove unnecessary additions

--- _site/sample_cli.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o errexit -o nounset
-
-HSAMPLE_TIMEOUT="${HSAMPLE_TIMEOUT:-150s}"
-
-HSAMPLE_ENDPOINT="${HSAMPLE_ENDPOINT:-https://raw.githubusercontent.com/fossasia/open-event/master/sample/OTS16}"
-
-node cli.js heroku_dir $HSAMPLE_ENDPOINT & timeout $HSAMPLE_TIMEOUT npm run start;
