fossasia/open-event-wsgen

commit c43e5c4f152000affe8b32e1668fe14f2530316a

date filter added on various pages (#2121)

src/backend/assets/js/main.js
@@ -203,9 +203,26 @@ $(document).ready(function () {
     });
   };
 
+  function viewsFilter(filterType, curView, date) {
+    const filter = allFilter[filterType];
+    if(curView === 'list') {
+      $(filter.dateFilter).each(function () {
+        const showThing = date === 'all' || $(this).hasClass(date);
+        $(this).toggleClass('hide', !showThing);
+      });
+    }
+    else if(curView === 'calendar') {
+      $(filter.dateFilter).each(function () {
+        const showThing = date === 'all' || $(this).hasClass(date);
+        $(this).toggleClass('hide', !showThing);
+      });
+    }
+  }
+
   window.main = {
     trackRoomFilter: trackRoomFilter,
     showNoResult: showNoResult,
+    viewsFilter: viewsFilter,
     handleClientLoad: handleClientLoad,
     initClient: initClient,
     handleAuthClick: handleAuthClick,

src/backend/templates/rooms.hbs
@@ -69,8 +69,9 @@
       <div class="date-list container">
           <div id="tabs" class="col-md-8 col-xs-12 col-sm-12 tabs">
             <span class="tabs-nav">
+              <a id="all" href="#" class="all date-button tabs-nav-link is-active">All</a>
               {{#days}}
-                <a href="./rooms.html#{{firstSlug}}" class="tabs-nav-link">{{caption}}</a>
+                <a href="#" class="{{firstSlug}} date-button tabs-nav-link">{{caption}}</a>
               {{/days}}
             </span>
           </div>
@@ -590,6 +591,14 @@
             main.trackRoomFilter('room', trackFilterMode, roomFilterMode, trackName, roomName, temp, mode);
             main.showNoResult('room', 0);
             $('html, body').animate({scrollTop: 0}, 500);
+        });
+
+        $('.date-button').click(function() {
+          let className = $(this).attr('class').split(' ')[0];
+          $(this).addClass('is-active');
+          $(this).siblings().removeClass('is-active');
+          main.viewsFilter('room', 'list', className);
+          main.showNoResult('room', false);
         });
 
         function display() {

src/backend/templates/schedule.hbs
@@ -880,37 +880,6 @@
         return className;
       }
 
-      function viewsFilter(curView, date) {
-        if(curView === 'list') {
-          $('.day-filter').each(function () {
-            if(date !== 'all') {
-              if ($(this).attr('class').split(' ')[0] === date) {
-                $(this).removeClass('hide');
-              } else {
-                $(this).addClass('hide');
-              }
-            }
-            else{
-              $(this).removeClass('hide');
-            }
-          });
-        }
-        else if(curView === 'calendar') {
-          $('.calendar').each(function () {
-            if(date !== 'all') {
-              if ($(this).attr('class').split(' ')[0] === date) {
-                $(this).removeClass('hide');
-              } else {
-                $(this).addClass('hide');
-              }
-            }
-            else{
-              $(this).removeClass('hide');
-            }
-          });
-        }
-      }
-
       $('.date-button').click(function() {
         let className = $(this).attr('class').split(' ')[0];
 
@@ -918,9 +887,9 @@
         $(this).siblings().removeClass('is-active');
 
         if(isCalendarView) {
-          viewsFilter('calendar', className);
+          main.viewsFilter('schedule', 'calendar', className);
         } else {
-          viewsFilter('list', className);
+          main.viewsFilter('schedule', 'list', className);
         }
         main.showNoResult('schedule', isCalendarView);
       });
@@ -941,14 +910,14 @@
           trackRoomNames.css('margin-top','0%');
           trackListId.css('margin-top', '0%');
           if (curDate !== '') {
-            viewsFilter('calendar', curDate);
+            main.viewsFilter('schedule', 'calendar', curDate);
           }
         } else {
           isCalendarView = false;
           trackRoomNames.css('margin-top','16%');
           trackListId.css('margin-top', '4%');
           if (curDate !== '') {
-            viewsFilter('list', curDate);
+            main.viewsFilter('schedule', 'list', curDate);
           }
         }
       });

src/backend/templates/tracks.hbs
@@ -61,8 +61,9 @@
       <div class="date-list container">
         <div id="tabs" class="col-md-8 col-xs-12 col-sm-12 tabs no-js">
           <span class="tabs-nav">
+            <a id="all" href="#" class="all date-button tabs-nav-link is-active">All</a>
             {{#days}}
-              <a href="./tracks.html#{{firstSlug}}" class="tabs-nav-link">{{caption}}</a>
+              <a href="#" class="{{firstSlug}} date-button tabs-nav-link">{{caption}}</a>
             {{/days}}
           </span>
         </div>
@@ -125,7 +126,7 @@
             <div id="track-list">
               {{#days}}
                <a class="anchor" id="{{firstSlug}}"></a>
-               <div class="date-filter">
+               <div class="{{firstSlug}} date-filter">
                 <div class="row paddingzero">
                   <div class="col-md-12 paddingzero">
                     <h5 class="text">{{caption}}</h4>
@@ -581,6 +582,14 @@
             var newUrl = curUrl.substring(0, curUrl.lastIndexOf('/') + 1) + 'sessions/session_' + id + ".html";
             window.location.href = newUrl;
           }
+        });
+
+        $('.date-button').click(function() {
+          let className = $(this).attr('class').split(' ')[0];
+          $(this).addClass('is-active');
+          $(this).siblings().removeClass('is-active');
+          main.viewsFilter('track', 'list', className);
+          main.showNoResult('track', false);
         });
 
         function display() {

commit 158e01138b18489e921346181b37290afce77e04

fix: Calendar view switcher
Broken in #2121

src/backend/assets/js/main.js
@@ -128,6 +128,7 @@ $(document).ready(function () {
 
     schedule: {
       dateFilter:'.day-filter',
+      calendarFilter: '.calendar',
       place:'.time-filter',
       room:'.schedule-track'
     }
@@ -212,7 +213,7 @@ $(document).ready(function () {
       });
     }
     else if(curView === 'calendar') {
-      $(filter.dateFilter).each(function () {
+      $(filter.calendarFilter).each(function () {
         const showThing = date === 'all' || $(this).hasClass(date);
         $(this).toggleClass('hide', !showThing);
       });