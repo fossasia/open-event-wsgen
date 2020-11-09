0916ae72140d82d82c8408532cecb83f64be74fa
Dhruv Jain <dhruv.jain93@gmail.com>
2020-11-09 10:27:36 +0530

    Ref: Use api as query param instead of apiendpoint, include space around if condition

--- src/www/js/autoGenerate.js
+++ src/www/js/autoGenerate.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     const urlParams = new URLSearchParams(window.location.search);
     const email = urlParams.get('email');
-    const apiendpoint = urlParams.get('apiendpoint');
-    if(email && apiendpoint){
+    const apiendpoint = urlParams.get('api');
+    if (email && apiendpoint) {
         $('#email').val(email);
         $('#apiendpoint').val(apiendpoint);
         $('#btnGenerate').trigger('click');
