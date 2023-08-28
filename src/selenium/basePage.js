4d3982b757d51bcd82eb7eeba8d52190a22c53b8
lthanhhieu <lthanhhieu@tma.com.vn>
2023-08-28 15:14:20 +0700

    FIX-2675: Remove twitter link

--- src/selenium/basePage.js
+++ src/selenium/basePage.js
@@ -188,8 +188,7 @@ const BasePage = {
       let brokenLinks = 0;
       let counter = 0;
 
-      links.forEach(function(link) {
-        
+      links.filter(link => !link.includes('twitter.com')).forEach(function(link) {
         request(link, function(error, response) {
           counter += 1;
           if (error || response.statusCode === 404) {
